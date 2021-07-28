b3f7a4f14b44750e9273a749fd0f84e90ef4f49b
Zack Dewan <zack.dewan@gmail.com>
2021-07-27 20:35:04 -0400

    Setup
    Setting up the the git and testing a commit.

---
+++ Question1.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+}
